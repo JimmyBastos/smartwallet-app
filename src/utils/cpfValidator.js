ee9063e937a1cf6265d670970674b03639881105
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-09-13 19:10:57 -0300

    CPF Validation

---
+++ src/utils/cpfValidator.js
@@ -0,0 +1,45 @@
+
+/**
+ * @brief Verifica se um dado valor é um cpf válido
+ * @param {String} value
+ * @returns {Boolean}
+ *
+ * @see http://www.receita.fazenda.gov.br/aplicacoes/atcta/cpf/funcoes.js
+ */
+
+const cpfValidator = value => {
+  const SIZE = 11
+
+  value = String(value).replace(/[^\d]/g, '')
+
+  if (value.length !== SIZE) return false
+
+  const fakeList = [...Array(SIZE)].map((val, idx) => idx.toString().repeat(SIZE))
+
+  if (fakeList.includes(value)) return false
+
+  var rest
+  var sum = 0
+
+  for (let i = 1; i <= 9; i++) { sum = sum + parseInt(value[i - 1]) * (SIZE - i) }
+
+  rest = (sum * 10) % SIZE
+
+  if ((rest === 10) || (rest === SIZE)) { rest = 0 }
+
+  if (rest !== parseInt(value[9])) { return false }
+
+  sum = 0
+
+  for (let i = 1; i <= 10; i++) { sum = sum + parseInt(value[i - 1]) * (12 - i) }
+
+  rest = (sum * 10) % SIZE
+
+  if ((rest === 10) || (rest === SIZE)) { rest = 0 }
+
+  if (rest !== parseInt(value[10])) { return false }
+
+  return true
+}
+
+export default cpfValidator
