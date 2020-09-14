543663e6589cc80f4a1bb29aaa08e066ee2c1416
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-09-13 23:44:06 -0300

    Remove rectbutton from base card container

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components/native'
 
-import { RectButton } from 'react-native-gesture-handler'
-
-export const Container = styled(RectButton)`
+export const Container = styled.View`
   border-radius: 10px;
   background: ${props => props.theme.colors.foreground};
   width: 100%;
