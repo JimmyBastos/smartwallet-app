0ad5ef1964e72543de9a621fc1810a56fb527feb
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-09-13 23:44:20 -0300

    Credit Card List

---
+++ src/components/CreditCard/index.js
@@ -0,0 +1,117 @@
+import React, { useCallback, useState } from 'react'
+
+import FeatherIcon from 'react-native-vector-icons/Feather'
+
+import PropTypes from 'prop-types'
+
+import headerBackground from '../../assets/background2.png'
+
+import { useNavigation } from '@react-navigation/native'
+
+import { Alert } from 'react-native'
+
+import {
+  Card,
+  SpacedRow,
+  Brand,
+  Action,
+  ActionText,
+  CardNumber,
+  OwnerName,
+  ExpirationDate
+} from './styles'
+
+import api from '../../services/api'
+
+const CreditCard = ({ id: cartao_id, bandeira, numero, nome, validade }) => {
+  const isEditable = useState(!!cartao_id)
+  const navigation = useNavigation()
+
+  const handleEditCreditCardNavigate = useCallback(
+    () => {
+      navigation.navigate('EditCreditCard', { cartao_id })
+    }, [navigation, cartao_id]
+  )
+
+  const handleEditCreditCardDelete = useCallback(() => {
+    Alert.alert(
+      'Deseja realmente excluir este cartão?',
+      'Todas as faturas associadas a ele também serão excluidas.',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        { text: 'OK', onPress: () => api.delete(`cartoes/${cartao_id}`) }
+      ],
+      { cancelable: false }
+    )
+  }, [cartao_id])
+
+  return (
+    <Card
+      source={headerBackground}
+      imageStyle={{ borderRadius: 8 }}
+    >
+      <SpacedRow>
+        <Brand>
+          {bandeira}
+        </Brand>
+
+        {isEditable && (<>
+          <Action onPress={handleEditCreditCardNavigate}>
+            <ActionText>
+            Editar
+            </ActionText>
+
+            <FeatherIcon
+              name="edit"
+              size={16}
+              color="#fff"
+            />
+          </Action>
+
+          <Action
+            onPress={handleEditCreditCardDelete}
+          >
+            <ActionText>
+              Excluir
+            </ActionText>
+
+            <FeatherIcon
+              name="trash-2"
+              size={16}
+              color="#fff"
+            />
+          </Action>
+        </>)}
+
+      </SpacedRow>
+
+      <CardNumber
+        value={numero}
+        type={'credit-card'}
+        options={{
+          obfuscated: true
+        }}
+      />
+
+      <SpacedRow>
+        <OwnerName>
+          {nome}
+        </OwnerName>
+
+        <ExpirationDate>
+          {validade}
+        </ExpirationDate>
+      </SpacedRow>
+    </Card>
+  )
+}
+
+CreditCard.propTypes = {
+  id: PropTypes.number,
+  bandeira: PropTypes.string.isRequired,
+  numero: PropTypes.string.isRequired,
+  nome: PropTypes.string.isRequired,
+  validade: PropTypes.string.isRequired
+}
+
+export default CreditCard
---
+++ src/components/CreditCard/styles.js
@@ -0,0 +1,56 @@
+
+import styled from 'styled-components'
+
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { TextMask } from 'react-native-masked-text'
+
+export const Card = styled.ImageBackground`
+  height: 200px;
+  padding: 16px;
+  justify-content: space-between;
+`
+
+export const Brand = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-family: ${({ theme }) => theme.font.medium};
+`
+
+export const SpacedRow = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Action = styled(TouchableOpacity)`
+  flex-direction: row;
+  align-items: center;
+`
+export const ActionText = styled.Text`
+  font-family: ${({ theme }) => theme.font.regular};
+  color: #fff;
+  margin-right: 10px;
+  padding-top: 3px;
+`
+
+export const CardNumber = styled(TextMask)`
+  font-family: ${({ theme }) => theme.font.semibold};
+  font-size: 22px;
+  letter-spacing: 4px;
+  color: #fff;
+`
+
+export const OwnerName = styled.Text`
+  font-size: 15px;
+  letter-spacing: 2px;
+  font-family: ${({ theme }) => theme.font.medium};
+  color: #fff;
+  text-transform: uppercase;
+  flex:7;
+`
+
+export const ExpirationDate = styled.Text`
+  flex: 1;
+  color: #fff;
+  alignSelf: flex-start;
+`
